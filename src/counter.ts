24f3027c1107a72401c98d7199fcbc710dc98982
ShingoTanikawa <shingo.tanikawa@o-n.co.jp>
2023-06-07 22:15:32 +0900

    1st

---
+++ src/counter.ts
@@ -0,0 +1,13 @@
+/*
+  setupCounter
+*/
+export function setupCounter(element: HTMLButtonElement) {
+  let counter = 0
+  const setCounter = (count: number) => {
+    counter = count
+    element.innerHTML = `count is ${counter}`
+  }
+  // increment
+  element.addEventListener('click', () => setCounter(counter + 1))
+  setCounter(0)
+}
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    cssMinify: false,
+    minify: false,
+    modulePreload: {
+      polyfill: false,
+    },
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        abc: resolve(__dirname, 'abc.html'),
+      },
+      output: {
+        assetFileNames: "assets/[name][extname]",
+        chunkFileNames: "assets/[name].js", 
+        entryFileNames: "assets/[name].js",
+      },
+    },
+  },
+});
